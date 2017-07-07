f5c11ccf4abc5b42aa97496bbc96dd424c930e66
xing609 <mingxing.gui@ifuifu.com>
2017-07-07 20:48:09 +0800

    修改首页UI

---
+++ etc/config.js
@@ -0,0 +1,4 @@
+export default {
+  basePath: 'http://apitest.ifuifu.com/', 
+  fileBasePath: 'http://apitest.ifuifu.com/', 
+}
--- pages/chat/index.js
+++ pages/chat/index.js
@@ -1,7 +1,43 @@
-var app = getApp()
-Page( {
-  data: {
-    cartImg: '../../images/cart-null.png',
-    tipWords: '暂无聊天记录'
-  }
+const App = getApp()
+var Api = require('../../utils/api.js');
+Page({
+    data: {
+      indicatorDots: true,
+      vertical: false,
+      autoplay: true,
+      interval: 3000,
+      duration: 1000,
+      pageCount:0,
+      currentPage:0,
+      resDesc: null,
+      loadingHidden: false,  // loading
+      resultList:[]
+    },
+    getChatList: function () {
+      var that = this;
+      wx.request({
+        method: 'POST',
+        url: Api.getChatList({
+          token: '689e700378944a8e941eeb9c9e31ac60',
+          page:1
+        }),
+        success: function (res) {
+          console.log(res);
+          that.setData({
+            currentPage: res.data.currentPage,
+            pageCount: res.data.pageCount,
+            resultList: res.data.resultList
+          })
+          setTimeout(function () {
+            that.setData({
+              loadingHidden: true
+            })
+          }, 1500)
+        }
+      })
+    },
+    onLoad: function () {
+      var that = this;
+      this.getChatList();
+    }
 })
